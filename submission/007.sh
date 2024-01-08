2f2ec9b3afe6122e69cc63ca9603163cdee613a6
LeoSpyke <44040856+LeoSpyke@users.noreply.github.com>
2024-01-08 14:28:27 +0000

    007.sh first snippet

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,10 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+hash=$(bitcoin-cli getblockhash 123321)
+block=$(bitcoin-cli getblock $hash)
+echo $block
+
+for txid in $(echo "$block" | jq -c -r '.tx[]'); do
+    echo "$txid"
+done
+
+# bitcoin-cli scantxoutset start '["addr()"]'
