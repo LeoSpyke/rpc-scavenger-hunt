ce598f8aa3e4584e833dc9640a8d3f70fb87f856
LeoSpyke <44040856+LeoSpyke@users.noreply.github.com>
2024-01-09 14:19:12 +0000

    007 continue

--- submission/007.sh
+++ submission/007.sh
@@ -1,10 +1,21 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
 hash=$(bitcoin-cli getblockhash 123321)
 block=$(bitcoin-cli getblock $hash)
-echo $block
 
 for txid in $(echo "$block" | jq -c -r '.tx[]'); do
-    echo "$txid"
+    echo "Transaction: $txid"
+    tx=$(bitcoin-cli getrawtransaction "$txid" 1)
+    echo $tx | jq 
+    while IFS= read -r line; do
+        echo $line
+        n=$(echo "$line" | jq -r '.n')
+        addr=$(echo "$line" | jq -r '.scriptPubKey.address')
+        echo "$n and $addr"        
+    done <<< "$(echo "$tx" | jq -c '.vout[]')"
+    echo ""
+    # for vout in $(echo "$tx" | jq -c -r '.vout'); do
+    #     echo "vout: $vout"
+    # done
 done
 
-# bitcoin-cli scantxoutset start '["addr()"]'
+# gettxspendingprevout
