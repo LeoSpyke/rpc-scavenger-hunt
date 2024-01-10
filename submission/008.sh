d7f509f5d35922033a51215b2dbfedabab764ea2
LeoSpyke <44040856+LeoSpyke@users.noreply.github.com>
2024-01-10 14:10:25 +0000

    add 008.sh solution

--- submission/008.sh
+++ submission/008.sh
@@ -1,2 +1,9 @@
 # Which public key signed input 0 in this tx:
-#   `e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163`
+tx="e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163"
+script=$(bitcoin-cli getrawtransaction "$tx" 1 | jq -rc '.vin[0].txinwitness | last')
+asm=$(bitcoin-cli decodescript $script | jq -r -c '.asm')
+regex="^(OP_IF )([^ ]*)(.*)"
+if [[ $asm =~ $regex ]]
+then
+    echo "${BASH_REMATCH[2]}"
+fi
